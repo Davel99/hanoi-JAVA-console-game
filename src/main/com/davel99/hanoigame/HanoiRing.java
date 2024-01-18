8da39f76c5a836b6b1c85245f194350352f2d769
davel99 <joelmp3@live.com>
2024-01-18 17:23:38 -0600

    Added ring class

--- src/main/com/davel99/hanoigame/HanoiRing.java
+++ src/main/com/davel99/hanoigame/HanoiRing.java
@@ -2,5 +2,11 @@ package main.com.davel99.hanoigame;
 
 public class HanoiRing {
 	public int size;
+	public String color;
+	
+	public HanoiRing(int size, String color) {
+		this.size = size;
+		this.color = color;
+	}
 
 }
