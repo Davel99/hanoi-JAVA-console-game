1b66406f9ee4ef4a978938dd0911cd299907e8b9
davel99 <joelmp3@live.com>
2024-01-18 17:22:32 -0600

    Added tower class

--- src/main/com/davel99/hanoigame/HanoiRing.java
+++ src/main/com/davel99/hanoigame/HanoiRing.java
@@ -1,5 +1,6 @@
 package main.com.davel99.hanoigame;
 
 public class HanoiRing {
+	public int size;
 
 }
--- src/main/com/davel99/hanoigame/HanoiTower.java
+++ src/main/com/davel99/hanoigame/HanoiTower.java
@@ -1,6 +1,26 @@
 package main.com.davel99.hanoigame;
 
+import java.util.LinkedList;
 
 public class HanoiTower {
+	public int id;
+	public LinkedList<HanoiRing> rings = new LinkedList<HanoiRing>();
+	
+	public HanoiTower(Integer id) {
+		this.id = id;
+	}
+	
+	public boolean addRing(HanoiRing ring) {
+		if(!this.checkRing(ring.size)) return false;
+		this.rings.addFirst(ring);
+		return true;
+		
+	}
+	
+	public boolean checkRing(int size) {
+		if(this.rings.isEmpty())return true;
+		if(size < this.rings.getFirst().size) return true;
+		return false;
+	}
 
 }
